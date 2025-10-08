a2e22f655973a2427af59065ca1deb47d80194c7
sparlingaidan <sparliai@dukes.jmu.edu>
2025-10-08 14:21:49 -0400

    Admin page
    Part 4 of the tutorial.

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import FoodItem, LogItem, Profile
 
 # Register your models here.
+
+
+@admin.register(FoodItem)
+class FoodItemAdmin(admin.ModelAdmin):
+    list_display = ('foodName', 'fdcId')
+
+
+@admin.register(LogItem)
+class LogItemAdmin(admin.ModelAdmin):
+    list_display = ('profile', 'date')
+
+
+admin.site.register(Profile)
