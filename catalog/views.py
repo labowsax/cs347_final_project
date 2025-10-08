5c1e5df07b4c5c4b4541b0c39a69182aa674b6d2
sparlingaidan <sparliai@dukes.jmu.edu>
2025-10-08 16:30:51 -0400

    Setup for Search Page.
    I have setup the search page, as well as linking it from the index page.  Index at http://127.0.0.1:8000/catalog/ search page at http://127.0.0.1:8000/catalog/search/.  I have made few changes to index and the base  html.

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Profile, FoodItem, LogItem
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_food_items = FoodItem.objects.all().count()
+    num_instances = LogItem.objects.all().count()
+    num_profiles = Profile.objects.all().count()
+
+
+
+    context = {
+        'num_food_items': num_food_items,
+        'num_instances': num_instances,
+        'num_profiles': num_profiles,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+
+def search(request):
+    """View function for search page of site."""
+
+    context = {
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'search.html', context=context)
